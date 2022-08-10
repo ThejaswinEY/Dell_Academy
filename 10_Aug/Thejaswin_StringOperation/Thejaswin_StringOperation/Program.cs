3ef75331974de1d647e70419a301cd1f105d22f4
Thejaswin K <Thejaswin.K@in.ey.com>
2022-08-10 12:16:11 +0530

    First commit

---
+++ 10_Aug/Thejaswin_StringOperation/Thejaswin_StringOperation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Thejaswin_StringOperation
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string name = "Thejaswin";
+            string name2 = "Kannan";
+            Console.WriteLine(name);
+            Console.WriteLine(name.Length);
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(string.Concat(name,name2)); 
+
+            
+        }
+    }
+}
